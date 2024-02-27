f8aacd84827a211280f01bc787f6a6858e4fbd95
hwang1607 <54609782+hwang1607@users.noreply.github.com>
2024-02-27 11:49:24 -0800

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,13 +6,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Driver's License Reader</h1>
-        <div>
-          <WebcamCapture />
+        <div style={{ padding: '10px' }}>
+          <h1>Driver's License Reader</h1>
+          <div>
+            <WebcamCapture />
+          </div>
         </div>
       </header>
     </div>
   );
 }
+
 
 export default App;
